79e48c5eceb0afe6af66fe18e812abdea1d291c9
jasba24 <johanbuitrago342@gmail.com>
2021-06-27 11:17:18 -0500

    add funcion to map the data

--- src/components/TableSensor/TableSensor.jsx
+++ src/components/TableSensor/TableSensor.jsx
@@ -3,7 +3,16 @@ import json from "../../data.json"
 import "../../assets/scss/components/tableSensor/tableSensor.scss"
 
 export default function TableSensor() {
+	const checkSensor = () => {
+		const data = json.data
+		data.map((d, i) => {
+			if (d.alert_damage || d.alert_ship) {
+				return console.log(Object.keys(json));
+			}
+		})
+	}
 	const data = Object.values(json)
+	checkSensor()
 	return (
 		<table className="tableSensor">
 			<thead className="tableSensor__head">
